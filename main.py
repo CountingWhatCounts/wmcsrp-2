3b3f514e0ee81329f176f2481596da046e653489
msd2 <marc.dunford@gmail.com>
2025-02-06 09:06:10 +0000

    include processing in main

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from dotenv import load_dotenv
 
 from src.download_data import download_from_gcs
 from src.helper_functions import get_config
-
+import src.preprocessing as preprocess
 
 
 if __name__ == '__main__':
@@ -12,6 +12,17 @@ if __name__ == '__main__':
 
 
     download_from_gcs(
-        bucket_name=config.gcp_bucket,
-        downloaded_data_dir=config.downloaded_data_dir
+        bucket_name=config.get('gcp_bucket'),
+        downloaded_data_dir=config.get('downloaded_data_dir')
     )
+
+
+    preprocess.ace_project_grants(config.get('downloaded_data_dir'), config.get('preprocessed_data_dir'))
+    preprocess.ace_npo_funding(config.get('downloaded_data_dir'), config.get('preprocessed_data_dir'))
+    preprocess.economic_data(config.get('downloaded_data_dir'), config.get('preprocessed_data_dir'))
+    preprocess.grant360_data(config.get('downloaded_data_dir'), config.get('preprocessed_data_dir'))
+    # preprocess.imd_data(config.get('downloaded_data_dir'), config.get('preprocessed_data_dir'))
+    # preprocess.cultural_infrastructure(config.get('downloaded_data_dir'), config.get('preprocessed_data_dir'))
+    # preprocess.wellbeing(config.get('downloaded_data_dir'), config.get('preprocessed_data_dir'))
+    # preprocess.yougov(config.get('downloaded_data_dir'), config.get('preprocessed_data_dir'))
+    # preprocess.census_data(config.get('downloaded_data_dir'), config.get('preprocessed_data_dir'))
