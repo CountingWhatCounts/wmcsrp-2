752ef693b1af33a2f7a95a9c55ec42a1b8047251
msd2 <marc.dunford@gmail.com>
2025-02-07 17:28:17 +0000

    stop duplicate logs

--- src/logger.py
+++ src/logger.py
@@ -4,7 +4,8 @@ import logging
 def setup_logger():
     logger = logging.getLogger("shared_logger")
     logger.setLevel(logging.DEBUG)
-
+    logger.propagate = False
+    
     if not logger.handlers:
         console_handler = logging.StreamHandler()
         console_handler.setLevel(logging.DEBUG)
