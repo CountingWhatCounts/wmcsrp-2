36e74e80a3c1373ef344db52155e5075567909c3
msd2 <marc.dunford@gmail.com>
2025-03-19 10:58:54 +0000

    skip empty directories

--- src/download_data.py
+++ src/download_data.py
@@ -1,9 +1,14 @@
 import os
+from google.cloud.storage.blob import Blob
 from .logger import logger
 
 
-def download_from_gcs(blob, downloaded_data_dir: str) -> None:
+def download_from_gcs(blob: Blob, downloaded_data_dir: str) -> None:
     """Downloads all files from a GCS bucket to a local directory."""
+
+    if blob.name.endswith("/"):
+        logger.info(f"Skipping directory blob: {blob.name}")
+        return
 
     if os.path.exists(os.path.join(downloaded_data_dir, blob.name)):
         logger.info(f"File already exists: {blob.name}")
