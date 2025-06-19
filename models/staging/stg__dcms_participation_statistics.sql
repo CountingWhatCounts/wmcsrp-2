3b317eeee8abc7fa93eadabba43bb082dab333f1
msd2 <marc.dunford@gmail.com>
2025-06-19 14:53:03 +0100

    add models for combined dcms participation figures

--- models/staging/stg__dcms_participation_statistics.sql
+++ models/staging/stg__dcms_participation_statistics.sql
@@ -1,17 +1,11 @@
 select
     CAST("Participation Type" as text) as participation_type,
     CAST("Response Group" as text) as response_group,
-    CAST("Response Breakdown " as text) as response_breakdown,
-    CAST("Change between 2022/23 and 2023/24" as text) as change_between_2022_2023_and_2024,
+    CAST("Response Breakdown" as text) as response_breakdown,
     CAST("Percentage of respondents 2023/24" as float) as percentage_of_respondents_2023_24,
     CAST("Percentage of respondents 2023/24 Lower estimate" as float) as percentage_of_respondents_2023_24_lower_estimate,
     CAST("Percentage of respondents 2023/24 Upper estimate" as float) as percentage_of_respondents_2023_24_upper_estimate,
     CAST("2023/24 No. of respondents" as int) as number_of_respondents_2023_24,
-    CAST("2023/24 Base" as int) as number_of_respondents_2023_24_base,
-    CAST("Percentage of respondents 2022/23" as float) as percentage_of_respondents_2022_23,
-    CAST("Percentage of respondents 2022/23 Lower estimate" as float) as percentage_of_respondents_2022_23_lower_estimate,
-    CAST("Percentage of respondents 2022/23 Upper estimate" as float) as percentage_of_respondents_2022_23_upper_estimate,
-    CAST("2022/23 No. of respondents" as int) as number_of_respondents_2022_23,
-    CAST("2022/23 Base" as int) as number_of_respondents_2022_23_base
+    CAST("2023/24 Base" as int) as number_of_respondents_2023_24_base
 from
     {{ source('preprocessed_data', 'raw__dcms_participation_statistics') }}
---
+++ models/warehouse/int__dcms_participation_lad_itl1_england.sql
@@ -0,0 +1,97 @@
+with
+
+itl as (
+	SELECT participation_type, response_group, response_breakdown, percentage_of_respondents_2023_24, number_of_respondents_2023_24
+	FROM {{ ref('int__dcms_participation_statistics') }}
+	where
+		participation_type in (
+			'Attended Cultural Events in Person',
+			'Participated in Creative Activities',
+			'Visited a heritage site in person at least once in the last 12 months',
+			'Visited a museum or gallery in person in the last 12 months',
+			'Visited a public library building or mobile library in person in the last 12 months'
+		)
+		and response_group in ('Region (ITL1 level)')
+),
+
+lad as (
+	SELECT participation_type, response_group, response_breakdown, percentage_of_respondents_2023_24, number_of_respondents_2023_24
+	FROM {{ ref('int__dcms_participation_statistics') }}
+	where
+		participation_type in (
+			'Attended Cultural Events in Person',
+			'Participated in Creative Activities',
+			'Visited a heritage site in person at least once in the last 12 months',
+			'Visited a museum or gallery in person in the last 12 months',
+			'Visited a public library building or mobile library in person in the last 12 months'
+		)
+		and response_group in ('Local authority district')
+		and response_breakdown in (
+			'Herefordshire, County of',
+			'Stafford',
+			'Lichfield',
+			'Wyre Forest',
+			'Stratford-on-Avon',
+			'Wolverhampton',
+			'Wychavon',
+			'Coventry',
+			'Shropshire',
+			'Warwick',
+			'Sandwell',
+			'Walsall',
+			'South Staffordshire',
+			'Tamworth',
+			'Bromsgrove',
+			'Birmingham',
+			'East Staffordshire',
+			'Redditch',
+			'Staffordshire Moorlands',
+			'Telford and Wrekin',
+			'North Warwickshire',
+			'Nuneaton and Bedworth',
+			'Rugby',
+			'Malvern Hills',
+			'Solihull',
+			'Cannock Chase',
+			'Dudley',
+			'Worcester',
+			'Newcastle-under-Lyme',
+			'Stoke-on-Trent'
+		)
+),
+
+total as (
+	SELECT participation_type, response_group,'England' as response_breakdown, percentage_of_respondents_2023_24, number_of_respondents_2023_24
+	FROM {{ ref('int__dcms_participation_statistics') }}
+	where
+		participation_type in (
+			'Attended Cultural Events in Person',
+			'Participated in Creative Activities',
+			'Visited a heritage site in person at least once in the last 12 months',
+			'Visited a museum or gallery in person in the last 12 months',
+			'Visited a public library building or mobile library in person in the last 12 months'
+		)
+		and response_group in ('Total')
+),
+
+combined as (
+	select * from itl
+	union all
+	select * from lad
+	union all
+	select * from total
+)
+
+select
+    CASE
+        WHEN response_breakdown = 'Herefordshire, County of' THEN 'Herefordshire'
+        ELSE response_breakdown
+    END as area,
+    MAX(percentage_of_respondents_2023_24) FILTER (WHERE participation_type = 'Attended Cultural Events in Person') AS dcms_attended_cultural_events,
+    MAX(percentage_of_respondents_2023_24) FILTER (WHERE participation_type = 'Participated in Creative Activities') AS dcms_participated_creative_activities,
+    MAX(percentage_of_respondents_2023_24) FILTER (WHERE participation_type = 'Visited a heritage site in person at least once in the last 12 months') AS dcms_visited_heritage_site,
+    MAX(percentage_of_respondents_2023_24) FILTER (WHERE participation_type = 'Visited a museum or gallery in person in the last 12 months') AS dcms_visited_museum_or_gallery,
+    MAX(percentage_of_respondents_2023_24) FILTER (WHERE participation_type = 'Visited a public library building or mobile library in person in the last 12 months') AS dcms_visited_library
+from combined
+group by response_breakdown
+order by response_breakdown
