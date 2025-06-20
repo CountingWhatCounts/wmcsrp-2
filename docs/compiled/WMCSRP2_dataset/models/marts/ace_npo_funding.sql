ad898cc912f032c0aab2015b69e3fff4e840091b
GitHub Action <action@github.com>
2025-06-20 09:15:16 +0000

    Update dbt docs [skip ci]

--- docs/compiled/WMCSRP2_dataset/models/marts/ace_npo_funding.sql
+++
@@ -1,1 +0,0 @@
-select * from "wmcsrp2"."public_warehouse"."int__ace_npo_funding_aggregated"
--- docs/compiled/WMCSRP2_dataset/models/marts/census_2021_data.sql
+++ docs/compiled/WMCSRP2_dataset/models/marts/census_2021_data.sql
@@ -3,9 +3,9 @@ select distinct
     area_code,
     census_question,
     answer,
-    CAST(count_of_answer as integer),
-    CAST(sample_size as integer),
+    CAST(count_of_answer as integer) as count_of_answer,
+    CAST(sample_size as integer) as sample_size,
     population_size,
-    CAST(p as float),
-    CAST(margin_of_error as float)
+    CAST(p as float) as p,
+    CAST(margin_of_error as float) as margin_of_error
 from "wmcsrp2"."public_warehouse"."int__census_with_error"
